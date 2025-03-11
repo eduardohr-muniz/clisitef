e9efe9c800d8ccce70cbda811ede4a88ef443bc1
Eduardo Muniz <90477293+eduardohr-muniz@users.noreply.github.com>
2025-03-11 12:52:24 -0300

    feat: update jar and libs

--- test/clisitef_test.dart
+++ test/clisitef_test.dart
@@ -1,27 +1,26 @@
+import 'package:clisitef/clisitef_method_channel.dart';
+import 'package:clisitef/clisitef_platform_interface.dart';
+import 'package:clisitef/flutter_clisitef.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:clisitef/clisitef.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-// class MockClisitefPlatform
-//     with MockPlatformInterfaceMixin
-//     implements ClisitefPlatform {
-//
-//   @override
-//   Future<String?> getPlatformVersion() => Future.value('42');
-// }
+class MockClisitefPlatform with MockPlatformInterfaceMixin implements ClisitefPlatform {
+  @override
+  Future<String?> getPlatformVersion() => Future.value('42');
+}
 
 void main() {
-  // final ClisitefPlatform initialPlatform = ClisitefPlatform.instance;
-  //
-  // test('$MethodChannelClisitef is the default instance', () {
-  //   expect(initialPlatform, isInstanceOf<MethodChannelClisitef>());
-  // });
-  //
-  // test('getPlatformVersion', () async {
-  //   Clisitef clisitefPlugin = Clisitef();
-  //   MockClisitefPlatform fakePlatform = MockClisitefPlatform();
-  //   ClisitefPlatform.instance = fakePlatform;
-  //
-  //   expect(await clisitefPlugin.getPlatformVersion(), '42');
-  // });
+  final ClisitefPlatform initialPlatform = ClisitefPlatform.instance;
+
+  test('$MethodChannelClisitef is the default instance', () {
+    expect(initialPlatform, isInstanceOf<MethodChannelClisitef>());
+  });
+
+  test('getPlatformVersion', () async {
+    FlutterClisitef clisitefPlugin = FlutterClisitef();
+    MockClisitefPlatform fakePlatform = MockClisitefPlatform();
+    ClisitefPlatform.instance = fakePlatform;
+
+    expect(await clisitefPlugin.getPlatformVersion(), '42');
+  });
 }
